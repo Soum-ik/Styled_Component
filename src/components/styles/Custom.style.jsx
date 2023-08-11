e157563e0e1fb02f3a857413e9ebc861023a4b07
Soumik Sarkar <sarkarsoumik215@gmail.com>
2023-08-11 12:16:40 +0600

    check

--- src/components/styles/Custom.style.jsx
+++ src/components/styles/Custom.style.jsx
@@ -4,3 +4,5 @@ import Custom from "../Custom";
 export const StyledCustom = styled(Custom)`
   background: ${(props) => props.color};
 `;
+// Hello world to check git hub specility checkout branch
+// Hello world to check git hub specility master branch
